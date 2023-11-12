4dce7c0d88aefb8a01f88b2ad52bc5ae87cbf460
AlfonsoGallardoFlorida <114494927+AlfonsoGallardoFlorida@users.noreply.github.com>
2023-11-12 01:07:23 +0100

    Add files via upload

---
+++ AvaluableBooks/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.florida.books","l":"Controlador"},{"p":"es.florida.books","l":"Model"},{"p":"es.florida.books","l":"Principal"},{"p":"es.florida.books","l":"Vista"}];updateSearchResults();
